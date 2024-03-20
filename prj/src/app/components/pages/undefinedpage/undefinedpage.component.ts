51ebdf1f2e3d6a70dde8efcc45ce665dff1dc62f
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-03-20 22:30:31 +0400

    fixed playBar problem

--- prj/src/app/components/pages/undefinedpage/undefinedpage.component.ts
+++ prj/src/app/components/pages/undefinedpage/undefinedpage.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-undefinedpage',
   templateUrl: './undefinedpage.component.html',
-  styleUrls: ['./undefinedpage.component.css']
+  styleUrls: ['./undefinedpage.component.css'],
 })
-export class UndefinedpageComponent {
-
-}
+export class UndefinedpageComponent {}
